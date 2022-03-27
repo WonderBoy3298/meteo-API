3adc94a188b1e605c3a55bfe2bf71c926316143b
hamza Wonderboy <achking.555@gmail.com>
2022-03-27 10:56:10 +0000

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+    function apiCall(city){
+
+   let APIKEY='1b6fe8a09da870658383ae266c2a7a37';
+   let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`;
+  
+fetch(url).then(Response=>Response.json())  
+    .then(datas=>{
+
+        console.log(datas);
+        document.querySelector("#city").innerHTML=datas.name;
+        document.querySelector("#temps").innerHTML="<i class='fa-solid fa-temperature-full'></i><br>"+datas.main.temp;
+        document.querySelector("#humidity").innerHTML="<i class='fa-solid fa-droplet-degree'></i><br>"+datas.main.humidity +'%';
+        document.querySelector("#wind").innerHTML="<i class='fa-solid fa-wind'></i><br>"+datas.wind.speed+'Km/h';
+
+    });
+}
+    document.querySelector("form").addEventListener('submit',function(e){
+
+        e.preventDefault();
+        let ville =  document.querySelector("#cp").value ;
+
+        apiCall(ville);
+        
+    });
